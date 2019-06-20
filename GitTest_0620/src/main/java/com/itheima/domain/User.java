63d085680d6eb8f7909603c086091697b4d696c3
qk <517269692@qq.com>
2019-06-20 08:15:18 +0800

    第一次提交至本地仓库

---
+++ GitTest_0620/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,16 @@
+package com.itheima.domain;
+
+import java.io.Serializable;
+
+/**
+ * @Author: QK
+ * @Date: 2019/6/20 8:05
+ * @Version 1.0
+ */
+public class User implements Serializable {
+    private Integer id;
+    private String userName;
+    private String password;
+    private String address;
+
+}
